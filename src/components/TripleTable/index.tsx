71974a1a7f3f6660c74823bd66682cb0bdd060ba
Jungmin Lee <mina.lee066@gmail.com>
2022-02-23 15:20:12 +0900

    Mobile download section added

--- src/components/TripleTable/index.tsx
+++ src/components/TripleTable/index.tsx
@@ -1,23 +1,18 @@
-import style from "./TripleTable.module.scss"
-
+import style from "./TripleTable.module.scss";
 
 type Props = {
-  id:string
-  title:string
-  children:React.ReactNode
+  id: string;
+  title: string;
+  children: React.ReactNode;
+};
+
+function TripleTable({ title, id, children }: Props) {
+  return (
+    <div className={style.page} id={id}>
+      <div className={style.pageHeader}>{title}</div>
+      <div className={style.table}>{children}</div>
+    </div>
+  );
 }
 
-function TripleTable({
-  title,
-  id,
-  children
-}:Props){
-  return <div className={style.page} id={id}>
-  <div className={style.pageHeader}>{title}</div>
-  <div className={style.table}>
-    {children}
-  </div>
-</div>
-}
-
-export default TripleTable
+export default TripleTable;
